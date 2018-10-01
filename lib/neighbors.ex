6331fafd5e4ca6c50314a174682912c34abaa2ce
jay137 <42896779+jay137@users.noreply.github.com>
2018-10-01 13:11:39 -0400

    line and full

--- lib/neighbors.ex
+++ lib/neighbors.ex
@@ -7,7 +7,7 @@ defmodule Neighbors do
     create_registry(1200) (number of nodes from the user input)
         Creates a registry starting from node 0 to the maximum possible number of nodes
     
-    get_neighbors("3d", 999, 10) (topology, maximum possible number of nodes for that topology, maximum number of coordinates for that topology)
+    get_neighbors("3D", 999, 10) (topology, maximum possible number of nodes for that topology, maximum number of coordinates for that topology)
         Gets neighbors for a particular node
         Returns a list
     '''
@@ -15,14 +15,14 @@ defmodule Neighbors do
     def main(topology, num) do
 
         case topology do
-            "3d" -> 
+            "3D" -> 
                 max_axis_coord = RC.nth_root(3, num) |> Kernel.trunc
                 possible_num_of_nodes = :math.pow(max_axis_coord,3) |> Kernel.trunc
-                Neighbors.create_registry("3d",possible_num_of_nodes, max_axis_coord)
+                Neighbors.create_registry("3D",possible_num_of_nodes, max_axis_coord)
 
                 Registry.start_link(keys: :unique, name: Registry.NeighReg)
                 for node <- 0..(possible_num_of_nodes-1) do
-                    neighbors = Neighbors.get_neighbors("3d",node, max_axis_coord)
+                    neighbors = Neighbors.get_neighbors("3D",node, max_axis_coord)
                     Registry.register(Registry.NeighReg, node, neighbors)
                 end
 
@@ -48,11 +48,36 @@ defmodule Neighbors do
 
                 create_registry("imp2D", randoms_remaining, num)
 
-                "line" -> 
+            "line" ->
+                Registry.start_link(keys: :unique, name: Registry.NeighReg) 
+                remaining = []
+                remaining = 
+                for i <- 0..(num-1) do 
+                    remaining++i
+                end
+                IO.inspect remaining
+                create_registry("line", remaining, num)
+            
+            "full" -> 
+                Registry.start_link(keys: :unique, name: Registry.NeighReg) 
+                nodes = []
+                nodes = 
+                for i <- 0..(num-1) do 
+                    nodes++i
+                end
+
+                for i <- 0..(num-1) do
+                    neighs = List.delete_at(nodes,i)
+                    Registry.register(Registry.NeighReg, i, neighs)
+                end
+
+            "rand2D" -> 
+
+                
         end
     end
 
-    def create_registry("3d",num, max_axis_coord) do
+    def create_registry("3D",num, max_axis_coord) do
     
         Registry.start_link(keys: :unique, name: Registry.CoordReg)
         for k <- 0..(max_axis_coord-1) do
@@ -102,7 +127,20 @@ defmodule Neighbors do
         create_registry("imp2D",remaining, num)
     end
 
-    def get_neighbors("3d", node, mac) do
+    def create_registry("line", remaining, _) when length(remaining)==0 do
+        IO.inspect "No more nodes"
+    end
+
+    def create_registry("line", remaining, num) do
+        [node_selected|remaining] = remaining
+        node_selected_neighs = get_neighbors("line", node_selected, num)
+
+        Registry.register(Registry.NeighReg, node_selected, node_selected_neighs)
+
+        create_registry("line",remaining, num)
+    end
+
+    def get_neighbors("3D", node, mac) do
         
         # node: <PID>
                 [{_,coord}] = Registry.lookup(Registry.CoordReg, node)
@@ -209,6 +247,19 @@ defmodule Neighbors do
         neighs
     end
 
+    def get_neighbors("line",node, no) do
+        neighs = [] 
+        neighs = 
+        cond do
+            node == 0 -> List.insert_at(neighs, -1, (node+1))
+            node == (no-1) -> List.insert_at(neighs, -1, (node-1))
+            true -> 
+                List.insert_at(neighs, -1, (node+1))
+                |> List.insert_at(-1, (node-1)) 
+        end
+        neighs
+    end
+
 end
 
 defmodule RC do
@@ -221,4 +272,7 @@ defmodule RC do
     defp fixed_point(f, _, tolerance, next), do: fixed_point(f, next, tolerance, f.(next))
 end
 
-
+Neighbors.main("full",10)
+for i <- 0..9 do
+    IO.inspect Registry.lookup(Registry.NeighReg, i), charlists: :as_lists
+end
