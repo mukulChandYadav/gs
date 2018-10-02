f7bd13802410bf570cab7f52cfe76251e5dab3a6
Mukul Chand Yadav <mchand.yadav@ufl.edu>
2018-10-01 21:56:57 -0400

    Fixed gossip infinite recursion

--- lib/gs/master.ex
+++ lib/gs/master.ex
@@ -15,13 +15,14 @@ defmodule GS.Master do
     commandline_arg = opts[:args]
 
     Logger.debug("commmand line_arg" <> inspect(commandline_arg))
-    [head|tail] = commandline_arg
+    [head | tail] = commandline_arg
     num_nodes = String.to_integer(head)
-    [head|tail] = tail
+    [head | tail] = tail
     topology = head
-    [head|tail] = tail
+    [head | tail] = tail
     algorithm = head
-    Logger.debug("commmand line_arg nodes - " <> inspect(num_nodes) <> " topology - " <> inspect(topology) <> " algo - "<> inspect(algorithm))
+
+    Logger.debug("commmand line_arg nodes - " <> inspect(num_nodes) <> " topology - " <> inspect(topology) <> " algo - " <> inspect(algorithm))
 
     # Create node registry <node_id, node_info>
     Registry.start_link(keys: :unique, name: GS.Registry.NodeInfo)
@@ -31,15 +32,23 @@ defmodule GS.Master do
     node_ids = Registry.keys(Registry.NeighReg, self())
     Logger.debug("Prepared node_ids" <> inspect(node_ids))
 
-    node_info_list = Enum.map(node_ids, fn x ->
-      {:ok, agent_node_pid} = GS.Node.start_link([x, algorithm, topology])
-      node_info = %GS.NodeInfo{node_id: x, termination_timestamp: 0, node_state: %{}, node_pid: agent_node_pid}
-      Agent.update(agent_node_pid, fn x -> 
-        %{x | node_pid: agent_node_pid}
-      end)
-      Registry.register(GS.Registry.NodeInfo, x, node_info)
-      node_info
-    end)
+    node_info_list = Enum.map(
+      node_ids,
+      fn x ->
+        {:ok, agent_node_pid} = GS.Node.start_link([x, algorithm, topology])
+        #node_info = %GS.NodeInfo{node_id: x, termination_timestamp: 0, node_state: %{}, node_pid: agent_node_pid}
+        Agent.update(
+          agent_node_pid,
+          fn x ->
+            %{x | node_pid: agent_node_pid}
+          end
+        )
+        node_info = Agent.get(agent_node_pid, fn x-> x end)
+
+        Registry.register(GS.Registry.NodeInfo, x, node_info)
+        node_info
+      end
+    )
 
     Logger.debug("node_list" <> inspect(node_info_list))
 
@@ -47,12 +56,28 @@ defmodule GS.Master do
     # Send message to random node
 
     #TODO: Capture current start timestamp
+    start_timestamp = :os.system_time(:millisecond)
 
     random_seed_node = Enum.random(node_info_list)
     Logger.debug("Calling random node " <> inspect(random_seed_node.node_pid))
 
-    GS.Node.send_msg(random_seed_node.node_id, master_pid, -1,algorithm, "abc")
-    {:ok, %{}}
+    msg =
+      case algorithm do
+        "gossip" ->
+          %{}
+
+        "push_sum" ->
+          random_seed_node.node_state
+      end
+
+    GS.Node.send_msg(random_seed_node.node_id, master_pid, algorithm, msg)
+
+    end_timestamp = :os.system_time(:millisecond)
+
+    time_interval = end_timestamp - start_timestamp
+    Logger.info("Time taken: "<> inspect(time_interval) <>"ms")
+
+    {:ok, %{time: time_interval}}
   end
 
   def handle_call(_msg, _from, state) do
--- lib/gs/node.ex
+++ lib/gs/node.ex
@@ -13,52 +13,46 @@ defmodule GS.Node do
     agent_id = head
     [head|tail] = tail
     algo = head
-    
+
     agent_node_info =
       case algo do
         "gossip" ->
-          %GS.NodeInfo{node_id: agent_id, termination_timestamp: 0, node_state: %{count: 0}, node_pid: -1}  
-        "push-sum" -> 
-          %GS.NodeInfo{node_id: agent_id, termination_timestamp: 0, node_state: %{s: agent_id, w: 1, count: 0}, node_pid: -1}
+          %GS.NodeInfo{node_id: agent_id, termination_timestamp: 0, node_state: %{count: 0}, node_pid: -1}
+        "push_sum" ->
+          %GS.NodeInfo{node_id: agent_id, termination_timestamp: 0, node_state: %{s: agent_id + 1, w: 1, count: 0}, node_pid: -1}
       end
     Logger.debug("Inside start_link agent node info " <> inspect(agent_node_info) )
     Agent.start_link(fn -> agent_node_info end)
   end
 
-  def send_msg(agent_id, master_pid, _from, algorithm, msg) do
+  def send_msg(agent_id, master_pid, algorithm, msg) do
 
-    Logger.debug("Received msg: " <> msg <> " for " <> inspect(agent_id) <> " with master : "<> inspect(master_pid) <> " from : "<> inspect(_from) <> " to: " <> inspect(self()))
-
-    #TODO: Get neighbors
-    #node_ids = Registry.keys(Registry.NeighReg, master_pid)
-
+    #Logger.debug("Received msg: " <> msg <> " for " <> inspect(agent_id) <> " with master : "<> inspect(master_pid) <> " from : "<> inspect(_from) <> " to: " <> inspect(self()))
+    Logger.debug("Received msg: " <> inspect(msg) <> " for " <> inspect(agent_id) <> " with master : "<> inspect(master_pid) <>" to: " <> inspect(self()))
     [{_, this_node_info}] = Registry.lookup(GS.Registry.NodeInfo, agent_id)
     Logger.debug("Info for this node" <> inspect(this_node_info))
 
     [{_, neighbors}] = Registry.lookup(Registry.NeighReg, agent_id)
-    Logger.debug("Neighbors for this node" <> inspect(neighbors))
+    #Logger.debug("Neighbors for this node" <> inspect(neighbors))
 
+    try do
     chosen_neighbor_node_info = Enum.map(
                              neighbors,
                              fn x ->
                                [{_, chosen_neighbor_node_info}] = Registry.lookup(GS.Registry.NodeInfo, x)
+                               #Logger.debug("Is Process: "<> inspect(chosen_neighbor_node_info.node_pid) <> "  alive: " <> inspect(Process.alive?(chosen_neighbor_node_info.node_pid)))
                                chosen_neighbor_node_info
                              end
                            )
                            |> Enum.filter(fn x -> Process.alive?(x.node_pid) end)
-                          #  |> Logger.debug()
                            |> Enum.random()
 
-
-    Logger.debug("chosen_neighbor_node  " <> inspect(chosen_neighbor_node_info))
-
-    #[{_, chosen_neighbor_node_info}] = Registry.lookup(GS.Registry.NodeInfo, chosen_neighbor_node)
-    #Logger.debug("chosen_neighbor_node_info " <> inspect(chosen_neighbor_node_info))
+    #Logger.debug("chosen_neighbor_node  " <> inspect(chosen_neighbor_node_info))
 
     [should_terminate, msg] =
       case algorithm do
         "gossip" ->
-          Logger.debug("In gossip")
+          #Logger.debug("In gossip")
           this_node_info = Agent.get(this_node_info.node_pid, fn x -> x end)
           this_node_state = this_node_info.node_state
 
@@ -67,21 +61,23 @@ defmodule GS.Node do
           Logger.debug("Count:" <> inspect(count))
 
           if count == 10 do
-            [true, nil]
+            Logger.debug("Count equals 10. Returning" <> inspect(count))
+            [true, %{}]
           else
+            Logger.debug("Buzz Count ne 10. Returning" <> inspect(count))
             count = count + 1
             updated_map = Map.put(this_node_state, :count, count)
-            Agent.update(this_node_info.node_pid, fn x -> 
+            Agent.update(this_node_info.node_pid, fn x ->
               %{x | node_state: updated_map}
             end)
             #TODO Has todo remove
             this_node_info = Agent.get(this_node_info.node_pid, fn x -> x end)
             this_node_state = this_node_info.node_state
             Logger.debug("This node state" <> inspect(this_node_state))
-           
+
+            [false, %{}]
           end
 
-          [false, nil]
 
         "push_sum" ->
           Logger.debug("In push sum")
@@ -91,7 +87,7 @@ defmodule GS.Node do
 
           s = Map.get(this_node_state, :s)
           w = Map.get(this_node_state, :w)
-        
+
           msg_s = Map.get(msg, :s)
           msg_w = Map.get(msg, :w)
 
@@ -101,47 +97,59 @@ defmodule GS.Node do
           old_sum_est = s/w
           new_sum_est = new_s/new_w
 
-          diff = :math.abs(old_sum_est - new_sum_est)
+          diff = abs(old_sum_est - new_sum_est)
+          Logger.debug("Diff : " <> inspect(diff))
 
-          count  = if diff < :math.pow(10, -10) do
-            Map.get(this_node_state, :count)+1
-          else
-            0
-              end
+          count  =
+            if diff < :math.pow(10, -10) do
+              Logger.debug("Diff lesser than threshold: " <> inspect(diff))
+              Map.get(this_node_state, :count) + 1
+            else
+              0
+            end
 
           if count == 3 do
-            [true, nil]
+            Logger.debug("Count reached 3 for: " <> inspect(this_node_info))
+            [true, %{}]
           else
             updated_map = Map.put(this_node_state, :count, count)
             updated_map = Map.put(this_node_state, :s, new_s/2)
             updated_map = Map.put(this_node_state, :w, new_w/2)
-            Agent.update(this_node_info.node_pid, fn x -> 
+            Agent.update(this_node_info.node_pid, fn x ->
               %{x | node_state: updated_map}
             end)
             #TODO Has todo remove
             this_node_info = Agent.get(this_node_info.node_pid, fn x -> x end)
             this_node_state = this_node_info.node_state
-            Logger.debug("This node state" <> inspect(this_node_state))
-           
+            Logger.debug("Updated node state" <> inspect(this_node_state))
+
+            [false, %{s: new_s/2, w: new_w/2}]
           end
-
-          [false, %{s: new_s/2, w: new_w/2}]
-
-        _ ->
-
-          [false, nil]
       end
 
-    #GS.Node.send_msg(chosen_neighbor_node_info.node_pid, master_pid, this_node_info.node_id, algorithm, msg)
+    #Logger.debug(" Chosen neighbor id" <> inspect(chosen_neighbor_node_info.node_id))
+
+    #Logger.debug(" This node " <> inspect(this_node_info.node_id) <> " with count: " <> inspect(this_node_info.node_state) <> " should terminate : " <> inspect(should_terminate))
 
     if should_terminate do
-      Logger.debug("Terminating " <> inspect(agent_id))
-      Process.exit(
-        agent_id,
-        :normal
-      )
+      Logger.debug("Terminating node id - " <> inspect(agent_id) <> " pid - " <> inspect(this_node_info.node_pid))
+      #TODO: Communicate to master about it's exit
+
+      Process.unlink(this_node_info.node_pid)
+      Agent.stop(this_node_info.node_pid)
+      #Process.exit(
+       # this_node_info.node_pid,
+        #:normal
+      #)
     end
- 
+
+    GS.Node.send_msg(chosen_neighbor_node_info.node_id, master_pid, algorithm, msg)
+
+    rescue
+      Enum.EmptyError ->
+
+    end
+
   end
 
 end
